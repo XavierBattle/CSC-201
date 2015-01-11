8323e14e7b3e1d06dbcac6c347e48eee268a56b1
Tom Peacock <peacockdt@gmail.com>
2015-01-11 17:55:07 -0500

    Create Cipher.java

---
+++ Cipher/Cipher.java
@@ -0,0 +1,18 @@
+
+/**
+ * Name: Tom Peacock
+ * Lab9 Exercise17 & Program9
+ * Date: 16 Apr 2013
+ * Description: Tester for ShufflerCipher
+ */
+public class Cipher 
+{
+
+	public static void main(String[] args) 
+	{
+		ShufflerCipher cipherTest = new ShufflerCipher(5);
+
+
+	}
+
+}
